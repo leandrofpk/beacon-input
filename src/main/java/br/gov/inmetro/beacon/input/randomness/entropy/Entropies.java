92b1bae9a4e8c138c735bdaeb2a8ad0192572a19
Leandro Pinho <leandro.fpk@gmail.com>
2019-10-16 14:33:30 -0300

    Repository

--- src/main/java/br/gov/inmetro/beacon/input/randomness/entropy/Entropies.java
+++ src/main/java/br/gov/inmetro/beacon/input/randomness/entropy/Entropies.java
@@ -1,9 +1,11 @@
 package br.gov.inmetro.beacon.input.randomness.entropy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface Entropies extends  JpaRepository<Entropy, Long>, IEntropyQueries {
     List<Entropy> findBySentOrderById(boolean sent);
 }
